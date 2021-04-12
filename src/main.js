93779b78aaa5e0b2ff789fe2864cf3581b1305e3
1939108122 <1939108122@qq.com>
2021-04-12 17:13:00 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import axios from 'axios';
+Vue.prototype.$axios = axios;
+
+
 Vue.config.productionTip = false
 
 new Vue({
